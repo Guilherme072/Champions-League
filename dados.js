8179053b4b1b9d9a146f7850f8f9fdd65fb905bd
Guilherme072 <111805132+Guilherme072@users.noreply.github.com>
2024-09-06 19:40:24 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,57 @@
+let dados = [
+    {
+      titulo: "Real Madrid",
+      descricao: "O Real Madrid é um dos clubes de futebol mais prestigiados e condecorados do mundo, com uma história rica e uma legião de fãs apaixonados. Fundado em 1902, o clube espanhol conquistou inúmeros títulos nacionais e internacionais, incluindo um recorde de 14 títulos da Liga dos Campeões da UEFA. Com um elenco estrelado e uma torcida fervorosa, o Real Madrid é sinônimo de sucesso e tradição no futebol mundial, sendo considerado por muitos o maior clube de todos os tempos.",
+      link: "https://pt.wikipedia.org/wiki/Real_Madrid_Club_de_F%C3%BAtbol",
+      tags: "galácticos, SantiagoBernabéu, LaLiga, ChampionsLeague, CristianoRonaldo, maior campeão"
+    },
+    {
+      titulo: "Milan",
+      descricao: "O Milan é um dos clubes de futebol mais tradicionais da Itália e da Europa, com uma história rica e uma legião de fãs apaixonados. Fundado em 1899, o clube italiano conquistou inúmeros títulos nacionais e internacionais, incluindo sete títulos da Liga dos Campeões da UEFA. Com um estilo de jogo elegante e uma torcida apaixonada, o Milan é sinônimo de sucesso e tradição no futebol italiano.",
+      link: "https://pt.wikipedia.org/wiki/AC_Milan",
+      tags: "SanSiro, SerieA, ChampionsLeague, Maldini, Nesta"
+    },
+    {
+      titulo: "Barcelona",
+      descricao: "O Barcelona é um dos clubes de futebol mais populares do mundo, conhecido por seu estilo de jogo ofensivo e sua famosa academia de base, La Masia. Fundado em 1899, o clube catalão conquistou inúmeros títulos nacionais e internacionais, incluindo cinco títulos da Liga dos Campeões da UEFA. Com um elenco estrelado e uma torcida apaixonada, o Barcelona é sinônimo de sucesso e tradição no futebol espanhol.",
+      link: "https://pt.wikipedia.org/wiki/FC_Barcelona",
+      tags: "CampNou, La Liga, ChampionsLeague, Messi, Guardiola, Neymar"
+    },
+    {
+      titulo: "Manchester United",
+      descricao: "O Manchester United é um dos clubes de futebol mais populares e bem-sucedidos da Inglaterra. Fundado em 1878, o clube inglês conquistou inúmeros títulos nacionais e internacionais, incluindo três títulos da Liga dos Campeões da UEFA. Com uma torcida apaixonada e uma história rica, o Manchester United é um dos clubes mais importantes do futebol mundial.",
+      link: "https://pt.wikipedia.org/wiki/Manchester_United_F.C.",
+      tags: "OldTrafford, PremierLeague, ChampionsLeague, Ferguson, Cantona"
+    },
+    {
+      titulo: "Liverpool",
+      descricao: "O Liverpool FC é um dos clubes de futebol mais populares e bem-sucedidos da Inglaterra. Fundado em 1892, o clube inglês conquistou inúmeros títulos nacionais e internacionais, incluindo seis títulos da Liga dos Campeões da UEFA. Com uma torcida apaixonada e uma história rica, o Liverpool FC é um dos clubes mais importantes do futebol mundial.",
+      link: "https://pt.wikipedia.org/wiki/Liverpool_F.C.",
+      tags: "Anfield, PremierLeague, ChampionsLeague, Klopp, Gerrard"
+    },
+    {
+      titulo: "Bayern de Munique",
+      descricao: "O Bayern de Munique é um dos clubes de futebol mais bem-sucedidos da Alemanha. Fundado em 1900, o clube alemão conquistou inúmeros títulos nacionais e internacionais, incluindo seis títulos da Liga dos Campeões da UEFA. Com um estilo de jogo eficiente e uma estrutura sólida, o Bayern de Munique é um dos clubes mais dominantes do futebol europeu.",
+      link: "https://pt.wikipedia.org/wiki/FC_Bayern_München",
+      tags: "AllianzArena, Bundesliga, ChampionsLeague, Neuer, Lewandowski"
+    },
+    {
+      titulo: "Ajax",
+      descricao: "O Ajax é um dos clubes de futebol mais tradicionais da Holanda. Fundado em 1900, o clube holandês conquistou inúmeros títulos nacionais e internacionais, incluindo quatro títulos da Liga dos Campeões da UEFA. Com uma famosa academia de base e um estilo de jogo ofensivo, o Ajax é conhecido por revelar grandes talentos para o futebol mundial.",
+      link: "https://pt.wikipedia.org/wiki/AFC_Ajax",
+      tags: "JohanCruijffArena, Eredivisie, ChampionsLeague, Cruyff, Overmars"
+    },
+    {
+      titulo: "Inter de Milão",
+      descricao: "A Internazionale de Milão, ou simplesmente Inter de Milão, é um dos clubes de futebol mais tradicionais da Itália. Fundado em 1908, o clube italiano conquistou inúmeros títulos nacionais e internacionais, incluindo três títulos da Liga dos Campeões da UEFA. Com uma torcida apaixonada e uma história rica, a Inter de Milão é um dos clubes mais importantes do futebol italiano.",
+      descrição: "https://pt.wikipedia.org/wiki/FC_Internazionale_Milano",
+      tags: "SanSiro, SerieA, ChampionsLeague, Mourinho, Ibrahimovic"
+    },
+    {
+      titulo: "Juventus",
+      descricao: "A Juventus Football Club, conhecida simplesmente como Juventus, é um dos clubes de futebol mais bem-sucedidos e populares da Itália. Fundado em 1897, o clube italiano conquistou inúmeros títulos nacionais e internacionais, incluindo dois títulos da Liga dos Campeões da UEFA. Com uma torcida apaixonada e uma história rica, a Juventus é um dos clubes mais importantes do futebol italiano.",
+      descrição: "https://pt.wikipedia.org/wiki/Juventus_F.C.",
+      tags: "AllianzStadium, SerieA, ChampionsLeague, Buffon, Pirlo"
+    }
+  ];
+ 
